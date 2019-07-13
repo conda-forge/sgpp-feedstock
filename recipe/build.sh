24a9d48c24c9d1736dea838897265e2d4f6dce00
Julien Schueller <schueller@phimeca.com>
2019-07-13 11:55:51 +0200

    Relink

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,3 +11,12 @@ export CXXFLAGS="${CXXFLAGS} -std=c++11"
 scons COMPILER=${COMPILER} CC=${CC} CXX=${CXX} CPPFLAGS="-I${SP_DIR}/numpy/core/include" BOOST_INCLUDE_PATH=${PREFIX}/include GSL_INCLUDE_PATH=${PREFIX}/include SG_JAVA=0 COMPILE_BOOST_TESTS=0 RUN_PYTHON_TESTS=0 USE_ARMADILLO=0 USE_EIGEN=0 -j${CPU_COUNT} PREFIX=${PREFIX} -Q install || cat config.log
 mv ${PREFIX}/lib/sgpp/* ${PREFIX}/lib
 cp -RLv lib/pysgpp ${SP_DIR}
+
+if test `uname` = "Darwin"
+then
+  for mod in datadriven combigrid optimization pde solver quadrature base
+  do
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${SP_DIR}/pysgpp/_pysgpp_swig.so
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgpp*.dylib
+  done
+fi
