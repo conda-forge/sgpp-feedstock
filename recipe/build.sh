8f54c101a2b2e449124102d8d1e29d4dd5f43fe5
Julien Schueller <schueller@phimeca.com>
2019-07-13 17:48:58 +0200

    Test examples

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,6 +17,16 @@ then
   for mod in datadriven combigrid optimization pde solver quadrature base
   do
     install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${SP_DIR}/pysgpp/_pysgpp_swig.so
-    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgpp*.dylib
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgppdatadriven.dylib
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgppcombigrid.dylib
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgppdatadriven.dylib
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgppoptimization.dylib
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgpppde.dylib
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgppsolver.dylib
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgppquadrature.dylib
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgppbase.dylib
   done
 fi
+
+python ./base/examples/quadrature.py
+python ./optimization/examples/optimization.py
