b8ac569cd3c65b1589eb3bfc26f13bdaa2c89903
Julien Schueller <schueller@phimeca.com>
2021-06-28 22:20:31 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,7 @@ fi
 
 scons COMPILER=${COMPILER} CC=${CC} CXX=${CXX} CPPFLAGS="-I${SP_DIR}/numpy/core/include" BOOST_INCLUDE_PATH=${PREFIX}/include GSL_INCLUDE_PATH=${PREFIX}/include SG_JAVA=0 COMPILE_BOOST_TESTS=0 RUN_PYTHON_TESTS=0 USE_ARMADILLO=0 USE_EIGEN=0 -j${CPU_COUNT} PREFIX=${PREFIX} -Q install || cat config.log
 #mv ${PREFIX}/lib/sgpp/* ${PREFIX}/lib
-cp -Lrv lib/pysgpp ${SP_DIR}
+cp -RLv lib/pysgpp ${SP_DIR}
 
 #if test `uname` = "Darwin"
 #then
